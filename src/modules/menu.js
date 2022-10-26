f2f78de6809c98bc6cb0d0db4cf961591029589b
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-26 21:25:25 +0700

    Выполнил усложненное домашнее задание 20 урока

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -6,6 +6,17 @@ const menu = () => {
     const menu = document.querySelector("menu");
     const closeBtn = menu.querySelector(".close-btn");
     const menuItems = menu.querySelectorAll("ul>li>a");
+    const mouseScroll = document.querySelector("main>a");
+
+    const smoothScroll = (e, item) => {
+        e.preventDefault(); //отключяем стандартную прокрутку якоря
+        const blockID = item.getAttribute('href').substr(1); //получаем id элемента, к которому нужно прокрутить
+
+        document.getElementById(blockID).scrollIntoView({ //получаем элемент по id, затем совершается прокрутка с заданными параметрами
+            behavior: 'smooth',
+            block: 'start'
+        })
+    }
 
     const handleMenu = () => {
         menu.classList.toggle("active-menu");
@@ -13,7 +24,14 @@ const menu = () => {
 
     menuBtn.addEventListener("click", handleMenu);
     closeBtn.addEventListener("click", handleMenu);
-    menuItems.forEach((item) => item.addEventListener("click", handleMenu));
+    menuItems.forEach((item) => item.addEventListener("click", (event) => {
+        handleMenu();
+        smoothScroll(event, item);
+    }));
+
+    mouseScroll.addEventListener("click", (event) => {
+        smoothScroll(event, mouseScroll);
+    })
 }
 
 export default menu;
