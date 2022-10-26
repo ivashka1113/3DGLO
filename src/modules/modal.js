e7eaea2a542c964a873811eceff95f280d900de7
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-26 21:09:02 +0700

    Выполнил домашнее задание 20 урока

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -3,10 +3,31 @@
 const modal = () => {
     const buttons = document.querySelectorAll(".popup-btn");
     const modal = document.querySelector(".popup");
-    const closeBtn = modal.querySelector(".popup-close")
+    const closeBtn = modal.querySelector(".popup-close");
+    modal.style.transform = 'translateY(100%)';
+    let count = 50;
+    let modalHandle;
 
-    buttons.forEach(item => item.addEventListener("click", () => modal.style.display = "block"));
+
+    buttons.forEach(item => item.addEventListener("click", () => {
+        modal.style.display = "block";
+        if (screen.width > 768)
+            modalShow();
+    }));
     closeBtn.addEventListener("click", () => modal.style.display = "none");
+
+    const modalShow = () => {
+        if (count > 0) {
+            count--;
+            modalHandle = requestAnimationFrame(modalShow);
+            modal.style.transform = `translateY(${-count * 3}%)`;
+        } else {
+            cancelAnimationFrame(modalHandle);
+        }
+
+    }
+
+
 }
 
 export default modal;
