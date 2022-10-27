0ac73b00b99139667f66bb9b928c5d3a9bfb9056
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-28 00:20:12 +0700

    Выполнил усложненное домашнее задание 21 урока

--- src/modules/formValid.js
+++ src/modules/formValid.js
@@ -5,22 +5,52 @@ const formValid = () => {
     const formItemsEmail = document.querySelectorAll("form input[type=email]");
     const formItemsTel = document.querySelectorAll("form input[type=tel]");
 
+    // formItemsText.forEach((formItemText) => {
+    //     formItemText.addEventListener("input", () => {
+    //         console.log("Сообщение");
+    //         formItemText.value = formItemText.value.replace(/[^а-яА-Я\s\-]+/, "");
+    //     })
+    // })
+
+    // formItemsEmail.forEach((formItemEmail) => {
+    //     formItemEmail.addEventListener("input", () => {
+    //         formItemEmail.value = formItemEmail.value.replace(/[^а-яА-Я\d\@\-\_\.\!\~\*\']+/, "");
+    //     })
+    // })
+
+    // formItemsTel.forEach((formItemTel) => {
+    //     formItemTel.addEventListener("input", () => {
+    //         formItemTel.value = formItemTel.value.replace(/[^\d\\(\\)\-]+/, "");
+    //     })
+    // })
+
     formItemsText.forEach((formItemText) => {
-        formItemText.addEventListener("input", () => {
+        formItemText.addEventListener("blur", () => {
             console.log("Сообщение");
             formItemText.value = formItemText.value.replace(/[^а-яА-Я\s\-]+/, "");
+            formItemText.value = formItemText.value.replace(/[\s]+/, " ");
+            formItemText.value = formItemText.value.replace(/[\-]+/, "-");
+            formItemText.value = formItemText.value.replace(/^[\s\-]+/, "");
+            formItemText.value = formItemText.value.replace(/( |^)[а-яА-Я]/g, function (x) {
+                return x.toUpperCase();
+            });
+
         })
     })
 
     formItemsEmail.forEach((formItemEmail) => {
-        formItemEmail.addEventListener("input", () => {
+        formItemEmail.addEventListener("blur", () => {
             formItemEmail.value = formItemEmail.value.replace(/[^а-яА-Я\d\@\-\_\.\!\~\*\']+/, "");
+            formItemEmail.value = formItemEmail.value.replace(/[\-]+/, "-");
+            formItemEmail.value = formItemEmail.value.replace(/^[\s\-]+/, "");
         })
     })
 
     formItemsTel.forEach((formItemTel) => {
-        formItemTel.addEventListener("input", () => {
+        formItemTel.addEventListener("blur", () => {
             formItemTel.value = formItemTel.value.replace(/[^\d\\(\\)\-]+/, "");
+            formItemTel.value = formItemTel.value.replace(/[\-]+/, "-");
+            formItemTel.value = formItemTel.value.replace(/^[\s\-]+/, "");
         })
     })
 
