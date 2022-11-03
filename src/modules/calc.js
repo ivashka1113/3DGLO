78a1654518b6ab2db09c8c8ceb948211c9bb9bed
Ivan Scherbakov <ivashka1113@mail.ru>
2022-11-03 19:04:59 +0700

    Выполнил обязательное задание 24 урока

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -7,8 +7,8 @@ const calc = (price = 100) => {
     const calcCount = document.querySelector(".calc-count");
     const calcDay = document.querySelector(".calc-day");
     const total = document.getElementById("total");
+    let totalValue = 0;
 
-    let id;
 
 
 
@@ -18,36 +18,16 @@ const calc = (price = 100) => {
         const calcCountValue = 1 + 0.1 * +calcCount.value;
         const calcDayValue = !calcDay.value ? 1 : +calcDay.value >= 10 ? 1 : +calcDay.value > 5 ? 1.5 : 2;
 
-        let oldValue = +total.textContent;
-        let totalValue = 0;
-
         if (calcTypeValue && calcSquareValue) {
             totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
-            let i = 0;
-            id = setInterval(() => {
-
-                if (totalValue > +total.textContent) {
-                    total.textContent = oldValue + i;
-                } else if (totalValue < +total.textContent) {
-                    total.textContent = oldValue - i;
-                } else if (totalValue === +total.textContent) {
-                    total.textContent = totalValue;
-                    oldValue = +total.textContent;
-                    clearInterval(id);
-                }
-
-                i = Math.abs(totalValue - total.textContent) > 10000 ? i += 1000 : Math.abs(totalValue - total.textContent) > 1000 ? i += 100 : Math.abs(totalValue - total.textContent) > 100 ? i += 10 : Math.abs(totalValue - total.textContent) > 10 ? i++ : Math.abs(totalValue - total.textContent) < 1 ? i = i + 0.1 : i = 0;
-
-            }, 1);
-
         } else {
-            total.textContent = totalValue;
+            totalValue = 0;
         }
+
+        total.textContent = totalValue;
     }
 
     calcBlock.addEventListener('input', () => {
-        clearInterval(id);
-        console.log("Калк")
         countCalc();
     })
 }
